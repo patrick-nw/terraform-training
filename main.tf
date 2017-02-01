d03f8e9e375dcdae3d6f0013ba05825b83d7a528
Patrick Salsbury <patrick@nebulaworks.com>
2017-01-31 16:46:24 -0800

    Update main.tf
    Generating an error intentionally.

--- main.tf
+++ main.tf
@@ -38,34 +38,3 @@ variable "aws_region" {
 variable "num_webs" {
   default = "2"
 }
-
-resource "aws_instance" "web" {
-  instance_type          = "t2.micro"
-  ami                    = "ami-30217250"
-  subnet_id              = "subnet-2b349873"
-  vpc_security_group_ids = ["sg-bcd874db"]
-  count                  = "${var.num_webs}"
-
-  tags {
-    Identity = "ttt-bat"
-    Location = "ttt-batcave"
-    Vehicle  = "ttt-batmobile"
-
-    #Aiming for:
-    #Name = "web 1/2"
-    Name = "web ${count.index+1}/${var.num_webs}"
-  }
-}
-
-#module "example" {
-#  source = "./example-module"
-#  command = "echo 'Goodbye World'"
-#}
-
-output "public_ip" {
-  value = ["${aws_instance.web.*.public_ip}"]
-}
-
-output "public_dns" {
-  value = ["${aws_instance.web.*.public_dns}"]
-}
